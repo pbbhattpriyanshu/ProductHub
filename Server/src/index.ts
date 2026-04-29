532e63eaf4499cdb42008c9a0819f9776999b5d8
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2026-04-29 14:21:29 +0530

    chore: install or update dependencies in node_modules

---
+++ Server/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { ENV } from "./config/env";
+
+const app = express();
+
+app.get("/api/health", (req, res) => {
+  res.json({
+    message: "Welcome to ProductHub API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth",
+    endpoints: {
+      users: "/api/users",
+      products: "/api/products",
+      comments: "/api/comments",
+    },
+  });
+});
+
+
+app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
