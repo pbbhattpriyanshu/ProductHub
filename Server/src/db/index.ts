2c86722e177418226184762d72cdf931f94316ae
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2026-04-30 20:45:58 +0530

    feat: initialize server project with Express, PostgreSQL, and Drizzle ORM configuration

---
+++ Server/src/db/index.ts
@@ -0,0 +1,27 @@
+import { drizzle } from "drizzle-orm/singlestore/driver";
+import { Pool } from "pg";
+import * as schema from "./schema";
+import { ENV } from "../config/env";
+
+
+if (!ENV.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not defined");
+}
+
+//TODO: Explain about pool
+//A pool manages multiple database connections and reuses them, which improves performance and scalability.
+//Instead of creating a new connection for each query, the pool maintains a set of open connections that can be used by multiple requests.
+
+const pool = new Pool({
+    connectionString: ENV.DATABASE_URL,
+});
+
+pool.on("connect", () => {
+    console.log("DATABASE Connected Successfully");
+});
+
+pool.on("error", (err) => {
+    console.log("DATABASE Connection Error: ", err);
+});
+
+export const db = drizzle(pool, { schema });        
