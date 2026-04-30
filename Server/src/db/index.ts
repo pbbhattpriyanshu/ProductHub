da31b198344fff175aeeb81fe270ba23b00efa46
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2026-04-30 21:10:08 +0530

    feat: initialize Drizzle ORM client and implement database CRUD query operations

--- Server/src/db/index.ts
+++ Server/src/db/index.ts
@@ -1,4 +1,4 @@
-import { drizzle } from "drizzle-orm/singlestore/driver";
+import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema";
 import { ENV } from "../config/env";
