668355ae5a44e6f8366547dc2f61756c33769a44
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2026-05-01 12:04:29 +0530

    initial setup - frontend

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import { SignInButton } from "@clerk/react"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
